d420af22d8474c9eb5fcee5092d1e1649b062297
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-01-03 19:56:26 +0000

    initial spring boot setup

---
+++ src/main/java/org/thetasinner/web/Service.java
@@ -0,0 +1,14 @@
+package org.thetasinner.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Service {
+    @RequestMapping("/hello")
+    public @ResponseBody  Data hello(@RequestParam(value="name", defaultValue="ThetaSinner") String name) {
+        return new Data(name);
+    }
+}
