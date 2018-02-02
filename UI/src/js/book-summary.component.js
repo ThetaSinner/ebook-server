3c7f68ac013d7897c5a1fff39da78c05d3017bfb
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-02-02 21:49:51 +0000

    add tooltips to book summar fields which often overflow

--- UI/src/js/book-summary.component.js
+++ UI/src/js/book-summary.component.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import * as _ from 'lodash';
+import $ from 'jquery';
 
-import { formatDateShort } from './formatter';
+import { formatDate, formatDateShort } from './formatter';
+import DisplayHelper from './display-helper.service';
 
 export default class BookSummary extends React.Component {
     constructor(props) {
@@ -9,20 +11,30 @@ export default class BookSummary extends React.Component {
     }
 
     render() {
+        const displayHelper = new DisplayHelper();
+
         const book = this.props.book;
+        const authors = displayHelper.toCommaList(book.authors);
+        const tags = displayHelper.toCommaList(book.metadata.tags);
         return (
             <div className="row es-book-row">
-                <div className="col-sm-2">{book.title}</div>
-                <div className="col-sm-2">{_.isArray(book.authors) ? _.join(book.authors, ', ') : book.authors}</div>
-                <div className="col-sm-2">{book.publisher}</div>
+                <div className="col-sm-2" data-toggle="tooltip" data-placement="bottom" title={book.title}>{book.title}</div>
+                <div className="col-sm-2" data-toggle="tooltip" data-placement="bottom" title={authors}>{authors}</div>
+                <div className="col-sm-2" data-toggle="tooltip" data-placement="bottom" title={book.publisher}>{book.publisher}</div>
                 <div className="col-sm-1">{formatDateShort(book.datePublished)}</div>
                 <div className="col-sm-2">{book.isbn}</div>
                 <div className="col-sm-1">{book.metadata.rating}</div>
-                <div className="col-sm-1">{_.isArray(book.metadata.tags) ? _.join(book.metadata.tags, ', ') : book.metadata.tags}</div>
+                <div className="col-sm-1" data-toggle="tooltip" data-placement="bottom" title={tags}>{tags}</div>
                 <div className="col-sm-1 text-center es-icon-button" aria-label="Expand Book" onClick={this.props.onExpandRow}>
                     <i className="material-icons" aria-hidden="true">{this.props.expanded ? 'expand_less' : 'expand_more'}</i>
                 </div>
             </div>
         );
+    }
+
+    componentDidMount() {
+        $(document).ready(function(){
+            $('[data-toggle="tooltip"]').tooltip();
+        });
     }
 }
