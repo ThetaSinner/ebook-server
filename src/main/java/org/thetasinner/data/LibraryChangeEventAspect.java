12a863086a60970161a118fbfc5a8bbde7788ad5
ThetaSinner <greatdjonfire@hotmail.co.uk>
2018-12-15 00:57:21 +0000

    tidy up the delete event asset

--- src/main/java/org/thetasinner/data/LibraryChangeEventAspect.java
+++ src/main/java/org/thetasinner/data/LibraryChangeEventAspect.java
@@ -1,12 +1,8 @@
 package org.thetasinner.data;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.thetasinner.web.events.ChangeEventData;
@@ -17,8 +13,6 @@ import static org.thetasinner.web.events.ChangeEventData.ChangeType.BookDeleted;
 @Aspect
 @Component
 public class LibraryChangeEventAspect {
-  private static final Logger LOG = LoggerFactory.getLogger(LibraryChangeEventAspect.class);
-
   private final LibraryChangeService libraryChangeService;
 
   @Autowired
