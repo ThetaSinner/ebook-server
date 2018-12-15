ba3ae218c7653d229dd67ea081504101277257cc
ThetaSinner <greatdjonfire@hotmail.co.uk>
2018-12-15 01:17:35 +0000

    create maintenance controller to support reports

---
+++ src/main/java/org/thetasinner/maintenance/MaintenanceService.java
@@ -0,0 +1,11 @@
+package org.thetasinner.maintenance;
+
+import org.springframework.stereotype.Service;
+import org.thetasinner.web.model.ReportModel;
+
+@Service
+public class MaintenanceService {
+  public ReportModel createReport(String libraryName) {
+    return null;
+  }
+}
---
+++ src/main/java/org/thetasinner/web/controller/MaintenanceController.java
@@ -0,0 +1,26 @@
+package org.thetasinner.web.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.thetasinner.maintenance.MaintenanceService;
+import org.thetasinner.web.model.ReportModel;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/maintenance")
+public class MaintenanceController {
+  private final MaintenanceService maintenanceService;
+
+  public MaintenanceController(MaintenanceService maintenanceService) {
+    this.maintenanceService = maintenanceService;
+  }
+
+  @RequestMapping(path = "/report", method = RequestMethod.GET)
+  public @ResponseBody ReportModel createReport(@RequestParam(name = "libraryName") String libraryName) {
+    return maintenanceService.createReport(libraryName);
+  }
+}
