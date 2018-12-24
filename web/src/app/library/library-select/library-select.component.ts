764649e5c012d69a792fb10795f4049411f9e244
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-12-24 15:15:16 +0000

    integrate fontawesome for icons

--- web/src/app/library/library-select/library-select.component.ts
+++ web/src/app/library/library-select/library-select.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faFolderOpen, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-library-select',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./library-select.component.scss']
 })
 export class LibrarySelectComponent implements OnInit {
+  faFolderOpen: IconDefinition = faFolderOpen;
 
   constructor() { }
 
--- web/src/app/library/library.module.ts
+++ web/src/app/library/library.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LibrarySelectComponent } from './library-select/library-select.component';
 import { LibraryRoutingModule } from './library-routing.module';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [LibrarySelectComponent],
   imports: [
     CommonModule,
+    FontAwesomeModule,
     LibraryRoutingModule
   ]
 })
