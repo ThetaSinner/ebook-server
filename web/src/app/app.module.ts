961d4840cd215af3209539b38aad8acadb594a9e
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-12-24 14:31:47 +0000

    integrate bootstrap into the app

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { LibraryModule } from './library/library.module';
 
 @NgModule({
@@ -12,6 +13,7 @@ import { LibraryModule } from './library/library.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
     LibraryModule
   ],
   providers: [],
