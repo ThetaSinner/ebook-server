b304c2002803c8c543841a94ea22eeec8da4c96c
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-12-24 00:34:57 +0000

    add test for book type breakdown counts

--- src/test/java/org/thetasinner/ebookserver/MaintenanceControllerTest.java
+++ src/test/java/org/thetasinner/ebookserver/MaintenanceControllerTest.java
@@ -14,6 +14,7 @@ import org.thetasinner.ebookserver.helper.TestDataHelper;
 import org.thetasinner.ebookserver.helper.TestInfrastructureHelper;
 import org.thetasinner.ebookserver.helper.UrlHelper;
 import org.thetasinner.web.model.BookAddRequest;
+import org.thetasinner.web.model.ReportModel;
 import org.thetasinner.web.model.RequestBase;
 
 import java.io.IOException;
@@ -48,6 +49,43 @@ public class MaintenanceControllerTest {
         var libraryName = testDataHelper.getCurrentMethodName();
         eBookTestClient.createLibrary(libraryName, port);
 
+        addWebLink(libraryName);
+        uploadBook(libraryName);
+
+        var report = getReportModel(libraryName);
+        assertNotNull(report.getMetrics());
+        assertEquals(2, report.getMetrics().getNumberOfBooks());
+    }
+
+    @Test
+    public void breakdownOfBookCountsByTypeIsGiven() {
+        var libraryName = testDataHelper.getCurrentMethodName();
+        eBookTestClient.createLibrary(libraryName, port);
+
+        addWebLink(libraryName);
+        uploadBook(libraryName);
+
+        var report = getReportModel(libraryName);
+        assertNotNull(report.getMetrics());
+        assertEquals(1, report.getMetrics().getNumberOfLocalManagedBooks());
+        assertEquals(0, report.getMetrics().getNumberOfLocalUnmanagedBooks());
+        assertEquals(1, report.getMetrics().getNumberOfWebLinkBooks());
+    }
+
+    private ReportModel getReportModel(String libraryName) {
+        var response = eBookTestClient.getReport(libraryName, port);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+
+        var report = response.getBody();
+        assertNotNull(report);
+        return report;
+    }
+
+    private void uploadBook(String libraryName) {
+        eBookTestClient.uploadBook(libraryName, "test-ebook/document.pdf", port);
+    }
+
+    private void addWebLink(String libraryName) {
         var bookAddRequest = new RequestBase<BookAddRequest>();
         bookAddRequest.setName(libraryName);
         BookAddRequest addRequest = new BookAddRequest();
@@ -55,15 +93,5 @@ public class MaintenanceControllerTest {
         addRequest.setType(BookAddRequest.Type.WebLink);
         addRequest.setUrl("https://github.com/ThetaSinner/ebook-server/blob/master/src/test/resources/test-ebook/document.pdf");
         eBookTestClient.createBook(bookAddRequest, port);
-
-        eBookTestClient.uploadBook(libraryName, "test-ebook/document.pdf", port);
-
-        var response = eBookTestClient.getReport(libraryName, port);
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-
-        var report = response.getBody();
-        assertNotNull(report);
-        assertNotNull(report.getMetrics());
-        assertEquals(2, report.getMetrics().getNumberOfBooks());
     }
 }
