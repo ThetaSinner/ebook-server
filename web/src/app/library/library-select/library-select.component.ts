075786477e3a9a839e00f0f263942254207d8fe3
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-12-25 18:39:30 +0000

    extract get libraries and re-render after create

--- web/src/app/library/library-select/library-select.component.ts
+++ web/src/app/library/library-select/library-select.component.ts
@@ -1,24 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { faFolderOpen, IconDefinition } from '@fortawesome/free-solid-svg-icons';
-import { LibraryDataService } from '../library-data/library-data.service';
 
 @Component({
   selector: 'app-library-select',
   templateUrl: './library-select.component.html',
   styleUrls: ['./library-select.component.scss']
 })
-export class LibrarySelectComponent implements OnInit {
+export class LibrarySelectComponent {
   faFolderOpen: IconDefinition = faFolderOpen;
 
-  libraryData: any;
+  @Input() libraryData: any;
 
-  constructor(private libraryDataService: LibraryDataService) { }
-
-  ngOnInit() {
-    const component = this;
-    const subscription = this.libraryDataService.getLibraries().subscribe(libraryData => {
-      component.libraryData = libraryData;
-      subscription.unsubscribe();
-    });
-  }
+  constructor() { }
 }
--- web/src/app/library/library-workspace/library-workspace.component.ts
+++ web/src/app/library/library-workspace/library-workspace.component.ts
@@ -7,14 +7,26 @@ import { LibraryDataService } from '../library-data/library-data.service';
   styleUrls: ['./library-workspace.component.scss']
 })
 export class LibraryWorkspaceComponent implements OnInit {
+  libraryData: any;
 
   constructor(private libraryDataService: LibraryDataService) { }
 
   ngOnInit() {
+    this.getLibraries();
   }
 
   createLibrary(libraryName: string) {
+    const component = this;
     const subscription = this.libraryDataService.createLibrary(libraryName).subscribe(_ => {
+      component.getLibraries();
+      subscription.unsubscribe();
+    });
+  }
+
+  private getLibraries() {
+    const component = this;
+    const subscription = this.libraryDataService.getLibraries().subscribe(libraryData => {
+      component.libraryData = libraryData;
       subscription.unsubscribe();
     });
   }
