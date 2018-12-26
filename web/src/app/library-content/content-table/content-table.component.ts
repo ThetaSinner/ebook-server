3b09639ae8a06039cf9da1acfa1a49543cd7e5b5
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-12-26 19:08:43 +0000

    port book content display from old app

--- web/src/app/library-content/content-table/content-table.component.ts
+++ web/src/app/library-content/content-table/content-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { faChevronDown, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-content-table',
@@ -6,6 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./content-table.component.scss']
 })
 export class ContentTableComponent implements OnInit {
+  faChevronDown: IconDefinition = faChevronDown;
+
   @Input() tableData$;
 
   constructor() { }
