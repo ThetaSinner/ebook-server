e1a887712cafdf3b4c4464b815c825a936273e7b
ThetaSinner <ThetaSinner@users.noreply.github.com>
2018-12-30 12:28:45 +0000

    add fields to the content edit form

--- web/src/app/library-content/content-edit/content-edit.component.ts
+++ web/src/app/library-content/content-edit/content-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-content-edit',
@@ -7,6 +9,9 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./content-edit.component.scss']
 })
 export class ContentEditComponent implements OnInit {
+  addIcon: IconDefinition = faPlus;
+  removeIcon: IconDefinition = faMinus;
+
   detailForm: FormGroup;
   @Input() detailData;
   
@@ -14,8 +19,41 @@ export class ContentEditComponent implements OnInit {
 
   ngOnInit() {
     this.detailForm = this.formBuilder.group({
-      title: ['']
+      title: [''],
+      isbn: [''],
+      publisher: [''],
+      description: [''],
+      rating: [''],
+      authors: this.formBuilder.array([
+        this.formBuilder.control('')
+      ]),
+      tags: this.formBuilder.array([
+        this.formBuilder.control('')
+      ])
     });
   }
 
+  get authorsControl() {
+    return this.detailForm.get('authors') as FormArray;
+  }
+
+  addAuthorInput() {
+    this.authorsControl.push(this.formBuilder.control(''));
+  }
+
+  removeAuthorInput(index: number) {
+    this.authorsControl.removeAt(index);
+  }
+
+  get tagsControl() {
+    return this.detailForm.get('tags') as FormArray;
+  }
+
+  addTagInput() {
+    this.tagsControl.push(this.formBuilder.control(''));
+  }
+
+  removeTagInput(index: number) {
+    this.tagsControl.removeAt(index);
+  }
 }
