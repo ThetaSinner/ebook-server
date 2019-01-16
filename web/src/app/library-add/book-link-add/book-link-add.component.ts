0ee9797e481f44e693f76cdd6318ed24dd5765d4
ThetaSinner <greatdjonfire@hotmail.co.uk>
2019-01-16 23:20:22 +0000

    implement add book link

--- web/src/app/library-add/book-link-add/book-link-add.component.ts
+++ web/src/app/library-add/book-link-add/book-link-add.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { ParamMap, ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { BookDataService } from 'src/app/library-content/book-data/book-data.service';
 
 @Component({
   selector: 'app-book-link-add',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-link-add.component.scss']
 })
 export class BookLinkAddComponent implements OnInit {
+  private libraryName: string;
+  addLinkForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private activeRoute: ActivatedRoute,
+    private bookDataService: BookDataService
+  ) { }
 
   ngOnInit() {
+    this.addLinkForm = this.formBuilder.group({
+      bookUrl: ['']
+    });
+
+    this.activeRoute.parent.paramMap.pipe(
+      map((params: ParamMap) => params.get('libraryName'))
+    ).subscribe(libraryName => {
+      this.libraryName = libraryName;
+    });
   }
 
+  handleAdd() {
+    const url = this.addLinkForm.value.bookUrl;
+    const sub = this.bookDataService.addBook(this.libraryName, url, 'WebLink').subscribe(() => {
+      this.addLinkForm.setControl('bookUrl', new FormControl(''))
+      sub.unsubscribe();
+    });
+  }
 }
