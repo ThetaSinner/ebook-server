06ec51e080d06d501e2a48e62b021a10c7452f68
ThetaSinner <greatdjonfire@hotmail.co.uk>
2019-01-28 23:55:37 +0000

    ui improvements and start pushing data down to the weblink fixes

--- web/src/app/library-report/report-fixing/fixing-workspace/fixing-workspace.component.ts
+++ web/src/app/library-report/report-fixing/fixing-workspace/fixing-workspace.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class FixingWorkspaceComponent implements OnInit {
   @Input() reportFixModel: any;
+  fixWebLinks: any;
 
   constructor() { }
 
   ngOnInit() {
+    console.log('got things', this.reportFixModel);
+
+    const report = this.reportFixModel.report;
+    if (report == null) {
+      return;
+    }
+
+    const unreachableBooksModel = report.unreachableBooksModel;
+    if (unreachableBooksModel && unreachableBooksModel.webLinks) {
+      this.fixWebLinks = unreachableBooksModel.webLinks;
+    }
   }
 }
--- web/src/app/library-report/report-fixing/web-link-fix/web-link-fix.component.ts
+++ web/src/app/library-report/report-fixing/web-link-fix/web-link-fix.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-web-link-fix',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./web-link-fix.component.scss']
 })
 export class WebLinkFixComponent implements OnInit {
+  @Input() fixWebLinks: any;
 
   constructor() { }
 
   ngOnInit() {
+    
   }
-
 }
